53f04fe059ff30498406e0ff767237a76f1fe3fe
hynseok <wefwef12e@naver.com>
2024-09-04 17:17:02 +0900

    feat: config 분리

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
   pub database: DatabaseSettings,
-  pub allication_port: u16,
+  pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -11,4 +11,13 @@ pub struct DatabaseSettings {
   pub port: u16,
   pub host: String,
   pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+  let settings = config::Config::builder()
+    .add_source(
+      config::File::new("config.yaml", config::FileFormat::Yaml)
+    )
+    .build()?;
+  settings.try_deserialize::<Settings>()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::TcpListener;
 use rust_template::startup::run;
+use rust_template::config::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let config = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", config.application_port);
+
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
