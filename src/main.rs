15cce25e049a32ab0a1f7b8fa0293dcc0bcb3ee0
hynseok <wefwef12e@naver.com>
2024-09-04 17:17:40 +0900

    feat: db pool 방식 db 커넥션 적용

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::net::TcpListener;
+use sqlx::PgPool;
 use rust_template::startup::run;
 use rust_template::config::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(
+            &config.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
